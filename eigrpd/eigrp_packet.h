99ba36521464015cda4480839cdae06b9ff6ae6e
janovic <janovic@orava.sk>
2014-10-26 10:17:39 +0100

    eigrpd: Several functions separated to new source files according to their purpose (Query/Reply/Update/Hello handling).

--- eigrpd/eigrp_packet.h
+++ eigrpd/eigrp_packet.h
@@ -269,50 +269,69 @@ struct eigrp_tlv_hdr_type
 #define EIGRP_EXTCOMM_VRR		0x8806 /* EIGRP Vecmet: Rsvd + (internal) Routerid */
 
 /*Prototypes*/
-extern int eigrp_read(struct thread *);
+extern int eigrp_read (struct thread *);
 extern int eigrp_write (struct thread *);
 
-extern struct eigrp_packet *eigrp_packet_new(size_t);
-extern struct eigrp_packet *eigrp_packet_duplicate(struct eigrp_packet *, struct eigrp_neighbor *);
-extern void eigrp_packet_free(struct eigrp_packet *);
-extern void eigrp_packet_delete(struct eigrp_interface *);
-extern void eigrp_packet_header_init(int, struct eigrp_interface *, struct stream *,
+extern struct eigrp_packet *eigrp_packet_new (size_t);
+extern struct eigrp_packet *eigrp_packet_duplicate (struct eigrp_packet *, struct eigrp_neighbor *);
+extern void eigrp_packet_free (struct eigrp_packet *);
+extern void eigrp_packet_delete (struct eigrp_interface *);
+extern void eigrp_packet_header_init (int, struct eigrp_interface *, struct stream *,
 				     u_int32_t, u_int32_t, u_int32_t);
 extern void eigrp_packet_checksum (struct eigrp_interface *, struct stream *, u_int16_t);
 
-extern struct eigrp_fifo *eigrp_fifo_new(void);
-extern struct eigrp_packet *eigrp_fifo_head(struct eigrp_fifo *);
-extern struct eigrp_packet *eigrp_fifo_tail(struct eigrp_fifo *);
-extern struct eigrp_packet *eigrp_fifo_pop(struct eigrp_fifo *);
-extern struct eigrp_packet *eigrp_fifo_pop_tail(struct eigrp_fifo *);
+extern struct eigrp_fifo *eigrp_fifo_new (void);
+extern struct eigrp_packet *eigrp_fifo_head (struct eigrp_fifo *);
+extern struct eigrp_packet *eigrp_fifo_tail (struct eigrp_fifo *);
+extern struct eigrp_packet *eigrp_fifo_pop (struct eigrp_fifo *);
+extern struct eigrp_packet *eigrp_fifo_pop_tail (struct eigrp_fifo *);
 extern void eigrp_fifo_push_head (struct eigrp_fifo *, struct eigrp_packet *);
-extern void eigrp_fifo_free(struct eigrp_fifo *);
-extern void eigrp_fifo_reset(struct eigrp_fifo *);
+extern void eigrp_fifo_free (struct eigrp_fifo *);
+extern void eigrp_fifo_reset (struct eigrp_fifo *);
 
-extern void eigrp_send_packet_reliably(struct eigrp_neighbor *);
-extern void eigrp_send_reply(struct eigrp_neighbor *, struct eigrp_neighbor_entry *);
+extern void eigrp_send_packet_reliably (struct eigrp_neighbor *);
 
-extern void eigrp_send_query(struct eigrp_neighbor *, struct eigrp_neighbor_entry *);
-extern void eigrp_query_send_all(struct eigrp *, struct eigrp_neighbor_entry *);
+extern struct TLV_IPv4_Internal_type *eigrp_read_ipv4_tlv (struct stream *);
+extern u_int16_t eigrp_add_internalTLV_to_stream (struct stream *, struct eigrp_neighbor_entry *);
 
-extern void eigrp_update_send(struct eigrp_interface *, struct eigrp_prefix_entry *);
-extern void eigrp_update_send_all(struct eigrp *, struct eigrp_prefix_entry *, struct eigrp_interface *);
-extern void eigrp_update_send_init(struct eigrp_neighbor *);
-
-extern struct TLV_IPv4_Internal_type *eigrp_read_ipv4_tlv(struct stream *);
-extern u_int16_t eigrp_add_internalTLV_to_stream(struct stream *, struct eigrp_neighbor_entry *);
-
-extern int eigrp_unack_packet_retrans(struct thread *);
-extern int eigrp_unack_multicast_packet_retrans(struct thread *);
+extern int eigrp_unack_packet_retrans (struct thread *);
+extern int eigrp_unack_multicast_packet_retrans (struct thread *);
 
 /*
  * untill there is reason to have their own header, these externs are found in
  * eigrp_hello.c
  */
-extern void eigrp_hello_send(struct eigrp_interface *);
-extern void eigrp_hello_send_ack(struct eigrp_neighbor *);
-extern void eigrp_hello_receive(struct eigrp *, struct ip *, struct eigrp_header *,
+extern void eigrp_hello_send (struct eigrp_interface *);
+extern void eigrp_hello_send_ack (struct eigrp_neighbor *);
+extern void eigrp_hello_receive (struct eigrp *, struct ip *, struct eigrp_header *,
 				struct stream *, struct eigrp_interface *, int);
-extern int  eigrp_hello_timer(struct thread *);
+extern int  eigrp_hello_timer (struct thread *);
+
+/*
+ * These externs are found in eigrp_update.c
+ */
+extern void eigrp_update_send (struct eigrp_interface *, struct eigrp_prefix_entry *);
+extern void eigrp_update_receive (struct eigrp *, struct ip *, struct eigrp_header *,
+                                struct stream *, struct eigrp_interface *, int);
+extern void eigrp_update_send_all (struct eigrp *, struct eigrp_prefix_entry *, struct eigrp_interface *);
+extern void eigrp_update_send_init (struct eigrp_neighbor *);
+extern void eigrp_update_send_EOT (struct eigrp_neighbor *);
+
+/*
+ * These externs are found in eigrp_query.c
+ */
+
+extern void eigrp_send_query (struct eigrp_neighbor *, struct eigrp_neighbor_entry *);
+extern void eigrp_query_receive (struct eigrp *, struct ip *, struct eigrp_header *,
+                                 struct stream *, struct eigrp_interface *, int);
+extern void eigrp_query_send_all (struct eigrp *, struct eigrp_neighbor_entry *);
+
+/*
+ * These externs are found in eigrp_reply.c
+ */
+extern void eigrp_send_reply (struct eigrp_neighbor *, struct eigrp_neighbor_entry *);
+extern void eigrp_reply_receive (struct eigrp *, struct ip *, struct eigrp_header *,
+                                 struct stream *, struct eigrp_interface *, int);
+
 
 #endif /* _ZEBRA_EIGRP_PACKET_H */
