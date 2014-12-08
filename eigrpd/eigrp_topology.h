46ddb321176c4cc920e4ef421d3a3891835ea6d1
Matthew54 <matejperina@gmail.com>
2014-12-08 15:32:35 +0100

    Rethinked and reworked DUAL FSM. It should be simplified and easier to modify. Prepared for future protocol independence.

--- eigrpd/eigrp_topology.h
+++ eigrpd/eigrp_topology.h
@@ -43,14 +43,16 @@ extern void eigrp_prefix_entry_delete (struct list *, struct eigrp_prefix_entry 
 extern void eigrp_neighbor_entry_delete (struct eigrp_prefix_entry *, struct eigrp_neighbor_entry *);
 extern void eigrp_topology_delete_all (struct list *);
 extern unsigned int eigrp_topology_table_isempty (struct list *);
-extern struct eigrp_prefix_entry *eigrp_topology_table_lookup (struct list *, struct prefix_ipv4 *);
-extern struct eigrp_neighbor_entry *eigrp_topology_get_successor (struct eigrp_prefix_entry *);
-extern struct eigrp_neighbor_entry *eigrp_topology_get_fsuccessor (struct eigrp_prefix_entry *);
+extern struct eigrp_prefix_entry *eigrp_topology_table_lookup_ipv4 (struct list *, struct prefix_ipv4 *);
+extern struct list *eigrp_topology_get_successor (struct eigrp_prefix_entry *);
+//extern struct eigrp_neighbor_entry *eigrp_topology_get_fsuccessor (struct eigrp_prefix_entry *);
 extern struct eigrp_neighbor_entry *eigrp_prefix_entry_lookup (struct list *, struct eigrp_neighbor *);
 extern void eigrp_topology_update_all_node_flags (struct eigrp *);
 extern void eigrp_topology_update_node_flags (struct eigrp_prefix_entry *);
 extern int eigrp_topology_update_distance ( struct eigrp_fsm_action_message *);
-extern int eigrp_topology_get_successor_count (struct eigrp_prefix_entry *);
+extern void eigrp_update_routing_table(struct eigrp_prefix_entry *);
+extern void eigrp_topology_neighbor_down(struct eigrp *, struct eigrp_neighbor *);
+//extern int eigrp_topology_get_successor_count (struct eigrp_prefix_entry *);
 /* Set all stats to -1 (LSA_SPF_NOT_EXPLORED). */
 /*extern void eigrp_lsdb_clean_stat (struct eigrp_lsdb *lsdb);
 extern struct eigrp_lsa *eigrp_lsdb_lookup_by_id (struct eigrp_lsdb *, u_char,
